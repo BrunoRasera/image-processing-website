1d3978f60d405701f98295c4decefd1160273b2e
Bruno Rasera <bruno.rasera@unifesp.br>
2022-12-17 20:06:04 -0300

    histogram working

--- script.js
+++ script.js
@@ -20,9 +20,9 @@
 //     frameFiltered.delete();
 // }
 
-function showHistogram(canvasName, src){
-    
-    // let src = image.clone();
+function showHistogram(canvasName, image) {
+
+    let src = image.clone();
     console.log(`entrou com canvas ${canvasName}`);
     let srcVec = new cv.MatVector();
     try {
@@ -30,7 +30,7 @@ function showHistogram(canvasName, src){
     } catch (error) {
         console.log(error)
     }
-    
+
     srcVec.push_back(src);
     let accumulate = false;
     let channels = [0];
@@ -38,7 +38,7 @@ function showHistogram(canvasName, src){
     let ranges = [0, 255];
     let hist = new cv.Mat();
     let mask = new cv.Mat();
-    let color = new cv.Scalar(255, 255, 255);
+    let color = new cv.Scalar(250, 250, 250);
     let scale = 2;
 
     // You can try more different parameters
@@ -52,12 +52,13 @@ function showHistogram(canvasName, src){
         let binVal = hist.data32F[i] * src.rows / max;
         let point1 = new cv.Point(i * scale, src.rows - 1);
         let point2 = new cv.Point((i + 1) * scale - 1, src.rows - binVal);
-        cv.rectangle(dst, point1, point2, color, cv.FILLED);
+        cv.rectangle(dst, point1, point2, color, 1);
     }
     console.log(canvasName);
     cv.imshow(canvasName, dst);
-    // src.delete(); 
-    // imgHist.delete(); srcVec.delete(); mask.delete(); hist.delete();
+
+    src.delete();
+    srcVec.delete(); mask.delete(); hist.delete();
 }
 
 let btnTest = document.getElementById('btn-teste');
@@ -71,98 +72,93 @@ let inputSample5 = document.getElementById('sample-image5');
 let inputSample6 = document.getElementById('sample-image6');
 
 
+
 inputElement.addEventListener('change', (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
-    let image = cv.imread(imgElement);
-    showHistogram('canvasHist',image);
+    imgElement.onload = function () {
+        let image = cv.imread(imgElement);
+        showHistogram('canvasHist', image);
 
-    let newImage = cv.imread(imgElement);
-    showHistogram('canvasHist2',newImage);
-    cv.imshow('canvasOutput', newImage);
+        // let newImage = cv.imread(imgElement);
+        // showHistogram('canvasHist2',newImage);
+        // cv.imshow('canvasOutput', newImage);
 
-    image.delete();
-    newImage.delete();
+        image.delete();
+        // newImage.delete();
+    }
+
 });
 
 inputSample1.addEventListener('click', () => {
     imgElement.src = '/images/bright_cat.jpg';
-    let image = cv.imread(imgElement);
-    showHistogram('canvasHist',image);
+    imgElement.onload = function () {
+        let image = cv.imread(imgElement);
+        showHistogram('canvasHist', image);
 
-    image.delete();
+        image.delete();
+    }
 });
 
 inputSample2.addEventListener('click', () => {
     imgElement.src = '/images/butterfly_gray.jpg';
-    let image = cv.imread(imgElement);
-    showHistogram('canvasHist',image);
+    imgElement.onload = function () {
+        let image = cv.imread(imgElement);
+        showHistogram('canvasHist', image);
 
-    image.delete();
+        image.delete();
+    }
 });
-
-document.getElementById('btn-salt-pepper').addEventListener('click', () => {
-    let newImage = cv.imread(imgElement);
-    thres = Number(document.getElementById('salt-pepper-prob').value);
-    console.log(thres);
-    cv.threshold(newImage, newImage, thres, 200, cv.THRESH_BINARY);
-    cv.imshow('canvasOutput', newImage);
-    showHistogram('canvasHist2',newImage);
-
-    newImage.delete();
-})
 
 inputSample3.addEventListener('click', () => {
     imgElement.src = '/images/head.jpg';
-    let image = cv.imread(imgElement);
-    showHistogram('canvasHist',image);
+    imgElement.onload = function () {
+        let image = cv.imread(imgElement);
+        showHistogram('canvasHist', image);
 
-    let newImage = cv.imread(imgElement);
-    showHistogram('canvasHist2',newImage);
-    cv.imshow('canvasOutput', newImage);
-
-    image.delete();
-    newImage.delete();
+        image.delete();
+    }
 });
 
 inputSample4.addEventListener('click', () => {
     imgElement.src = '/images/lena_color.jpg';
-    let image = cv.imread(imgElement);
-    showHistogram('canvasHist',image);
+    imgElement.onload = function () {
+        let image = cv.imread(imgElement);
+        showHistogram('canvasHist', image);
 
-    let newImage = cv.imread(imgElement);
-    showHistogram('canvasHist2',newImage);
-    cv.imshow('canvasOutput', newImage);
-
-    image.delete();
-    newImage.delete();
+        image.delete();
+    }
 });
 
 inputSample5.addEventListener('click', () => {
     imgElement.src = '/images/retina.jpg';
-    let image = cv.imread(imgElement);
-    showHistogram('canvasHist',image);
+    imgElement.onload = function () {
+        let image = cv.imread(imgElement);
+        showHistogram('canvasHist', image);
 
-    let newImage = cv.imread(imgElement);
-    showHistogram('canvasHist2',newImage);
-    cv.imshow('canvasOutput', newImage);
-
-    image.delete();
-    newImage.delete();
+        image.delete();
+    }
 });
 
 inputSample6.addEventListener('click', () => {
     imgElement.src = '/images/sunflowers.jpg';
-    let image = cv.imread(imgElement);
-    showHistogram('canvasHist',image);
+    imgElement.onload = function () {
+        let image = cv.imread(imgElement);
+        showHistogram('canvasHist', image);
 
-    let newImage = cv.imread(imgElement);
-    showHistogram('canvasHist2',newImage);
-    cv.imshow('canvasOutput', newImage);
-
-    image.delete();
-    newImage.delete();
+        image.delete();
+    }
 });
 
+
+document.getElementById('btn-salt-pepper').addEventListener('click', () => {
+    let newImage = cv.imread(imgElement);
+    thres = Number(document.getElementById('salt-pepper-prob').value);
+    cv.threshold(newImage, newImage, thres, 200, cv.THRESH_BINARY);
+    cv.imshow('canvasOutput', newImage);
+    showHistogram('canvasHist2', newImage);
+
+    newImage.delete();
+})
 
 // imgElement.onload = function () {
 //     let mat = cv.imread(imgElement);
