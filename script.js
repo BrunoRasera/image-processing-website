0e8dcdeb529eb664e227de28cae65f0f298b3345
Bruno Rasera <bruno.rasera@unifesp.br>
2022-12-16 22:56:08 -0300

    histogram

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // const cv = require("./libs/opencv460");
 
 // const canvas = document.getElementById("canvas-output");
@@ -19,6 +21,38 @@
 //     frameFiltered.delete();
 // }
 
+function showHistogram(mat){
+    
+    let srcVec = new cv.MatVector();
+    cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY, 0);
+    srcVec.push_back(mat);
+    let accumulate = false;
+    let channels = [0];
+    let histSize = [256];
+    let ranges = [0, 255];
+    let hist = new cv.Mat();
+    let mask = new cv.Mat();
+    let color = new cv.Scalar(255, 255, 255);
+    let scale = 2;
+
+    // You can try more different parameters
+    cv.calcHist(srcVec, channels, mask, hist, histSize, ranges, accumulate);
+    let result = cv.minMaxLoc(hist, mask);
+    let max = result.maxVal;
+    let dst = new cv.Mat.zeros(mat.rows, histSize[0] * scale, cv.CV_8UC3);
+
+    // draw histogram
+    for (let i = 0; i < histSize[0]; i++) {
+        let binVal = hist.data32F[i] * mat.rows / max;
+        let point1 = new cv.Point(i * scale, mat.rows - 1);
+        let point2 = new cv.Point((i + 1) * scale - 1, mat.rows - binVal);
+        cv.rectangle(dst, point1, point2, color, cv.FILLED);
+    }
+    cv.imshow('canvasHist', dst);
+    src.delete(); imgHist.delete(); srcVec.delete(); mask.delete(); hist.delete();
+}
+
+
 let imgElement = document.getElementById('imageSrc');
 let inputElement = document.getElementById('fileInput');
 let inputSample1 = document.getElementById('sample-image1');
@@ -28,76 +62,107 @@ let inputSample4 = document.getElementById('sample-image4');
 let inputSample5 = document.getElementById('sample-image5');
 let inputSample6 = document.getElementById('sample-image6');
 
+
 inputElement.addEventListener('change', (e) => {
     imgElement.src = URL.createObjectURL(e.target.files[0]);
-}, false);
+    let mat = cv.imread(imgElement);
+    cv.imshow('canvasOutput', mat);
+    showHistogram(mat);
+    mat.delete();
+});
 
 inputSample1.addEventListener('click', () => {
     imgElement.src = '/images/bright_cat.jpg';
-}, false);
+    let mat = cv.imread(imgElement);
+    cv.imshow('canvasOutput', mat);
+    showHistogram(mat);
+    mat.delete();
+});
 
 inputSample2.addEventListener('click', () => {
     imgElement.src = '/images/butterfly_gray.jpg';
-}, false);
+    let mat = cv.imread(imgElement);
+    cv.imshow('canvasOutput', mat);
+    showHistogram(mat);
+    mat.delete();
+});
 
 inputSample3.addEventListener('click', () => {
     imgElement.src = '/images/head.jpg';
-}, false);
+    let mat = cv.imread(imgElement);
+    cv.imshow('canvasOutput', mat);
+    showHistogram(mat);
+    mat.delete();
+});
 
 inputSample4.addEventListener('click', () => {
     imgElement.src = '/images/lena_color.jpg';
-}, false);
+    let mat = cv.imread(imgElement);
+    cv.imshow('canvasOutput', mat);
+    showHistogram(mat);
+    mat.delete();
+});
 
 inputSample5.addEventListener('click', () => {
-    imgElement.src = '/images/retina.jpg'
-}, false);
+    imgElement.src = '/images/retina.jpg';
+    let mat = cv.imread(imgElement);
+    cv.imshow('canvasOutput', mat);
+    showHistogram(mat);
+    mat.delete();
+});
 
 inputSample6.addEventListener('click', () => {
     imgElement.src = '/images/sunflowers.jpg';
-}, false);
-
-
-imgElement.onload = function () {
     let mat = cv.imread(imgElement);
     cv.imshow('canvasOutput', mat);
+    showHistogram(mat);
     mat.delete();
-};
+});
 
-inputSample1.onload = function () {
-    let mat = cv.imread(imgElement);
-    cv.imshow('canvasOutput', mat);
-    mat.delete();
-};
 
-inputSample2.onload = function () {
-    let mat = cv.imread(imgElement);
-    cv.imshow('canvasOutput', mat);
-    mat.delete();
-};
+// imgElement.onload = function () {
+//     let mat = cv.imread(imgElement);
+//     cv.imshow('canvasOutput', mat);
+//     mat.delete();
+// };
 
-inputSample3.onload = function () {
-    let mat = cv.imread(imgElement);
-    cv.imshow('canvasOutput', mat);
-    mat.delete();
-};
+// inputSample1.onclick = function () {
+//     imgElement.src = '/images/bright_cat.jpg';
+//     let mat = cv.imread(imgElement);
+//     cv.imshow('canvasOutput', mat);
+//     showHistogram(mat);
+//     mat.delete();
+// };
 
-inputSample4.onload = function () {
-    let mat = cv.imread(imgElement);
-    cv.imshow('canvasOutput', mat);
-    mat.delete();
-};
+// inputSample2.onclick = function () {
+//     let mat = cv.imread(imgElement);
+//     cv.imshow('canvasOutput', mat);
+//     mat.delete();
+// };
 
-inputSample5.onload = function () {
-    let mat = cv.imread(imgElement);
-    cv.imshow('canvasOutput', mat);
-    mat.delete();
-};
+// inputSample3.onclick = function () {
+//     let mat = cv.imread(imgElement);
+//     cv.imshow('canvasOutput', mat);
+//     mat.delete();
+// };
 
-inputSample6.onload = function () {
-    let mat = cv.imread(imgElement);
-    cv.imshow('canvasOutput', mat);
-    mat.delete();
-};
+// inputSample4.onclick = function () {
+//     let mat = cv.imread(imgElement);
+//     cv.imshow('canvasOutput', mat);
+//     mat.delete();
+// };
+
+// inputSample5.onclick = function () {
+//     let mat = cv.imread(imgElement);
+//     cv.imshow('canvasOutput', mat);
+//     mat.delete();
+// };
+
+// inputSample6.onclick = function () {
+//     let mat = cv.imread(imgElement);
+//     cv.imshow('canvasOutput', mat);
+//     mat.delete();
+// };
 
 // var Module = {
 //     // https://emscripten.org/docs/api_reference/module.html#Module.onRuntimeInitialized
